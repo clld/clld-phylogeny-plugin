b68781e32d6e14120e30ff8ac87cd0c98d83b7bc
xrotwang <xrotwang@googlemail.com>
2020-11-02 14:24:54 +0100

    bump version for development

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clld-phylogeny-plugin',
-    version='1.5.0',
+    version='1.5.1.dev0',
     description='clld-phylogeny-plugin',
     classifiers=[
         "Programming Language :: Python",
